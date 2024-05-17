1204488174e6aa883b39a8331b667cadb415c86f
shuhei tanaka <shuhei46491@gmail.com>
2024-05-17 14:56:22 +0900

    fix db

--- worker/src/models.ts
+++ worker/src/models.ts
@@ -2,12 +2,13 @@ import { ColumnType, Generated } from 'kysely'
 import z from 'zod'
 
 export const ZodName = z.object({
+  id: z.number().int().optional(),
   name: z.string().regex(/^[a-z0-9-.]+$/),
   owner: z.string(),
   addresses: z.record(z.string()).optional(),
   texts: z.record(z.string()).optional(),
   contenthash: z.string().optional(),
-  status: z.enum(['PENDING', 'APPROVED', 'REJECTED']),
+  status: z.string(),
 })
 
 export const ZodNameWithSignature = ZodName.extend({
@@ -21,13 +22,12 @@ export type Name = z.infer<typeof ZodName>
 export type NameWithSignature = z.infer<typeof ZodNameWithSignature>
 
 export interface NameInKysely {
-  id: number
   name: string
   owner: string
   addresses: string | null // D1 doesn't support JSON yet, we'll have to parse it manually
   texts: string | null // D1 doesn't support JSON yet, we'll have to parse it manually
   contenthash: string | null
-  status: 'PENDING' | 'APPROVED' | 'REJECTED'
+  status: string
   createdAt: ColumnType<Date, never, never>
   updatedAt: ColumnType<Date, never, string | undefined>
 }
